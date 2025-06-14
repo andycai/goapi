c91c20ec30ae3232958940a7e68f14c4379fff9f
Andy Cai <huayicai@gmail.com>
2025-06-14 23:54:01 +0800

    Create code.go

---
+++ enum/code/code.go
@@ -0,0 +1,12 @@
+package code
+
+const (
+	Success              = 0
+	ErrorInvalidData     = -1
+	ErrorInvalidParam    = -2
+	ErrorInvalidToken    = -3
+	ErrorInvalidUser     = -4
+	ErrorInvalidPassword = -5
+	ErrorInvalidEmail    = -6
+	ErrorInvalidPhone    = -7
+)
